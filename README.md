ab61a7e268cb7740461c30deaf0bac97933e138c
reby0318 <rebecca860318@gmail.com>
2025-06-21 01:19:34 -0400

    README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TechChaser
+#this is a comment
--- helloworld.py
+++ helloworld.py
@@ -1,99 +1,107 @@
 #This is a comment
 x=10#variable 
 print(x)
-item = 'Banana'
-Item = 'Apple'
-item_name = 'Orange'
-print(item, Item)
-print('Hello'+item + item_name)
-interger = 2025
-word='text'
+
+
+# item = 'Banana'
+# Item = 'Apple'
+# item_name = 'Orange'
+# print(item, Item)
+# print('Hello'+item + item_name)
+# integer = 2025
+# word='text'
+# isHappy=False
+# naughty_list=['luigi', 'mario','Toad']
+# print (inte0ger)
+# print (word)
+# print (isHappy)
+# print(naughty_list) 
+
+# integer = 2025
+# name='Mario'
+# string_number='22'
+# print(name+str(integer))
+
+# print(10 + int(string_number))
+
+
+# a=10
+# b=5
+
+# addtion=a+b 
+# difference=a-b 
+# multiply=a*b
+# divide=a/b
+# power=a**b
+
+# print(addtion)
+# print(difference)
+# print(multiply)
+# print(divide)
+# print(power)
+
+# # This is a simple Python script to demonstrate variable usage and basic arithmetic operations.
+# # It defines several variables, performs arithmetic operations, and prints the results. 
+
+# age=28
 isHappy=False
-naughty_list=['luigi', 'mario','Toad']
-print (interger)
-print (word)
-print (isHappy)
-print(naughty_list) 
 
-interger = 2025
-name='Mario'
-string_number='22'
-print(name+str(interger))
-
-print(10 + int(string_number))
-
-
-a=10
-b=5
-
-addtion=a+b 
-difference=a-b 
-multiply=a+b
-divide=a/b
-power=a**b
-
-print(addtion)
-print(difference)
-print(multiply)
-print(divide)
-print(power)
-
-# This is a simple Python script to demonstrate variable usage and basic arithmetic operations.
-# It defines several variables, performs arithmetic operations, and prints the results. 
-
-age=28
-isHappy=False
-
-if age>21:
-    print('You are old!.')
-elif age==18:
-    print('You are getting old.')
-else:
-    print('You are still young!')
+# if age>21:
+#     print('You are old!.')
+# elif age==18:
+#     print('You are getting old.')
+# else:
+#     print('You are still young!')
 
 if isHappy:
     print('You are happy!')
+
 else:
     print('You are not happy!')
 
-    for i in range(3):
-        print('Hello', i+1)
-#In programming the index always starts at 0
+#     for i in range(3):
+#         print('Hello', i+1)
+# #In programming the index always starts at 0
 
-print(range(3))
-#this works for lists.
+# print(range(3))
+# #this works for lists.
 
-i=0
-while i <5:
-    i=i+1
-    print(1)
+# i=0
+# while i <5:
+#     i=i+1
+#     print(1)
 
-while True:
-    user_input = input('Enter something >>')
+# while True:
+#     user_input = input('Enter something >>')
 
-    if user_input=='0':
-        print('We are done here.')
-        break
+#     if user_input=='0':
+#         print('We are done here.')
+#         break
 
 def say_hello(name):
     print('Hey there ', name)
 
-say_hello('Luigi')
-say_hello('Mario')
-#reuse your code as much as possible or we have to write it again and again..
+# say_hello('Luigi')
+# say_hello('Mario')
+# #reuse your code as much as possible or we have to write it again and again..
 
-def get_internet():
-    pass
+# def get_internet():
+#     pass
 
-def run_game():
-    pass
-#when dont know what to write, use pass
-# This is a placeholder function for future implementation
-    # You can add code here to fetch internet data or perform other tasks
+# def run_game():
+#     pass
+# #when dont know what to write, use pass
+# # This is a placeholder function for future implementation
+#     # You can add code here to fetch internet data or perform other tasks
 
 
-number=input('please [provide a number>>"')
-try:
-    print(10+int(number))
-except:
-    print('That is not a valid number.')
+# number=input('please [provide a number>>"')
+# try:
+#     print(10+int(number))
+# except:
+#     print('That is not a valid number.')
+
+
+
+#     just_greeting()
+#     welcome_greeting()
